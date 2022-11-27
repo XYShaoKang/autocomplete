80748295e5b92ab001523fc86b4e9511a87e4fdd
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2022-11-27 13:26:00 +0800

    fix: add css style

--- src/Autocomplete.stories.tsx
+++ src/Autocomplete.stories.tsx
@@ -1,4 +1,6 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { within, userEvent } from '@storybook/testing-library'
+
 import Autocomplete from './Autocomplete'
 
 export default {
@@ -16,4 +18,10 @@ export const Defalut = Template.bind({})
 export const ProvideOptions = Template.bind({})
 ProvideOptions.args = {
   options: ['123', '124', '125', '234', '2345'],
+}
+ProvideOptions.play = async ({ canvasElement }): Promise<void> => {
+  const canvas = within(canvasElement)
+
+  const textbox = canvas.getByRole('textbox')
+  await userEvent.type(textbox, '1', { delay: 100 })
 }
--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -1,32 +1,108 @@
-import { ChangeEventHandler, FC, useState } from 'react'
+import { ChangeEventHandler, FC, useState, useMemo } from 'react'
+import { css } from 'styled-components'
 
 interface AutocompleteProps {
   options?: string[]
   placeholder?: string
 }
 
-const Autocomplete: FC<AutocompleteProps> = ({
-  options = [],
-  placeholder,
-} = {}) => {
+const Autocomplete: FC<AutocompleteProps> = ({ options = [], placeholder }) => {
   const [value, setValue] = useState('')
+
+  const showOptions = useMemo(
+    () => (value ? options.filter(v => v.indexOf(value) === 0) : []),
+    [value, options]
+  )
+
   const handleChange: ChangeEventHandler<HTMLInputElement> = e => {
     setValue(e.target.value)
   }
-  const showOptions = value ? options.filter(v => v.indexOf(value) === 0) : []
 
   return (
-    <div>
-      <input
-        type="text"
-        value={value}
-        onChange={handleChange}
-        placeholder={placeholder}
-      />
-      {showOptions && (
-        <ul>
+    <div
+      css={css`
+        position: relative;
+        width: 200px;
+        box-sizing: border-box;
+      `}
+    >
+      <div
+        css={css`
+          border-radius: 4px;
+          margin: 0;
+          padding: 0;
+          border: 1px #d9d9d9 solid;
+          height: 32px;
+          outline: none;
+          width: 100%;
+          box-sizing: border-box;
+          color: rgba(0, 0, 0, 0.88);
+          cursor: pointer;
+          &:hover,
+          &:focus-within {
+            border-color: #4096ff;
+          }
+          &:focus-within {
+            box-shadow: 0 0 0 2px #0591ff1a;
+          }
+        `}
+      >
+        <input
+          type="text"
+          value={value}
+          onChange={handleChange}
+          placeholder={placeholder}
+          css={css`
+            box-sizing: border-box;
+            width: 100%;
+            margin: 0;
+            background: transparent;
+            border: none;
+            outline: none;
+            appearance: none;
+            height: 30px;
+            cursor: auto;
+            padding: 0 11px;
+          `}
+        />
+      </div>
+      {!!showOptions.length && (
+        <ul
+          css={css`
+            position: absolute;
+            margin: 4px 0 0;
+            padding: 8px;
+            box-shadow: 0 6px 16px 0 rgb(0 0 0 / 8%),
+              0 3px 6px -4px rgb(0 0 0 / 12%), 0 9px 28px 8px rgb(0 0 0 / 5%);
+            width: 100%;
+            border-radius: 8px;
+            box-sizing: border-box;
+          `}
+        >
           {showOptions.map(v => (
-            <li key={v}>{v}</li>
+            <li
+              key={v}
+              css={css`
+                margin: 0;
+                padding: 0;
+                list-style: none;
+                min-height: 32px;
+                padding: 5px 12px;
+                color: rgba(0, 0, 0, 0.88);
+                font-size: 14px;
+                box-sizing: border-box;
+                cursor: pointer;
+                transition: background 0.3s ease;
+                border-radius: 4px;
+                display: flex;
+
+                &:hover {
+                  background-color: rgba(0, 0, 0, 0.04);
+                }
+              `}
+            >
+              {v}
+            </li>
           ))}
         </ul>
       )}
